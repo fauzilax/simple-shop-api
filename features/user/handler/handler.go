db4590d16b9cb218d59aada1ca97a999a9c68772
Fauzi Sofyan <fauzilax@gmail.com>
2023-03-14 03:13:57 +0700

    CHORE: updating structure folder user (handler,services)

--- features/user/handler/handler.go
+++ features/user/handler/handler.go
@@ -1,1 +1,37 @@
 package handler
+
+import (
+	"simple-shop-api/features/user"
+
+	"github.com/labstack/echo/v4"
+)
+
+type userHandlerController struct {
+	srv user.UserService
+}
+
+func New(us user.UserService) user.UserHandler {
+	return &userHandlerController{
+		srv: us,
+	}
+}
+
+// Delete implements user.UserHandler
+func (uhc *userHandlerController) Delete() echo.HandlerFunc {
+	panic("unimplemented")
+}
+
+// Login implements user.UserHandler
+func (uhc *userHandlerController) Login() echo.HandlerFunc {
+	panic("unimplemented")
+}
+
+// Register implements user.UserHandler
+func (uhc *userHandlerController) Register() echo.HandlerFunc {
+	panic("unimplemented")
+}
+
+// Update implements user.UserHandler
+func (uhc *userHandlerController) Update() echo.HandlerFunc {
+	panic("unimplemented")
+}
--- features/user/services/service.go
+++ features/user/services/service.go
@@ -1,1 +1,33 @@
 package services
+
+import "simple-shop-api/features/user"
+
+type userServiceCase struct {
+	query user.UserData
+}
+
+func New(ud user.UserData) user.UserService {
+	return &userServiceCase{
+		query: ud,
+	}
+}
+
+// Delete implements user.UserService
+func (usc userServiceCase) Delete(token interface{}, userID uint) error {
+	panic("unimplemented")
+}
+
+// Login implements user.UserService
+func (usc userServiceCase) Login(email string, password string) (user.UserCore, error) {
+	panic("unimplemented")
+}
+
+// Register implements user.UserService
+func (usc userServiceCase) Register(newUser user.UserCore) (user.UserCore, error) {
+	panic("unimplemented")
+}
+
+// Update implements user.UserService
+func (usc userServiceCase) Update(token interface{}, updateData user.UserCore) (user.UserCore, error) {
+	panic("unimplemented")
+}
