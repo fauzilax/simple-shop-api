81c7363d148ba31e071b4c37f8f12c637772b6a3
Fauzi Sofyan <fauzilax@gmail.com>
2023-03-14 02:25:02 +0700

    BUILD: config and db

--- config/config.go
+++ config/config.go
@@ -1,1 +1,75 @@
 package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/spf13/viper"
+)
+
+var (
+	JWTKey = ""
+)
+
+type DBConfig struct {
+	DBUser string
+	DBPass string
+	DBHost string
+	DBName string
+	DBPort int
+	JWTKey string
+}
+
+func InitConfig() *DBConfig {
+	return ReadEnv()
+}
+
+func ReadEnv() *DBConfig {
+	app := DBConfig{}
+	isRead := true
+
+	if val, found := os.LookupEnv("DBUSER"); found {
+		app.DBUser = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBPASS"); found {
+		app.DBPass = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBHOST"); found {
+		app.DBHost = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBNAME"); found {
+		app.DBName = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBPORT"); found {
+		temp, _ := strconv.Atoi(val)
+		app.DBPort = temp
+		isRead = false
+	}
+	if val, found := os.LookupEnv("JWT_KEY"); found {
+		app.JWTKey = val
+		isRead = false
+	}
+	if isRead {
+		viper.AddConfigPath(".")
+		viper.SetConfigName("local")
+		viper.SetConfigType("env")
+		err := viper.ReadInConfig()
+		if err != nil {
+			log.Println("error read config", err.Error())
+			return nil
+		}
+		err = viper.Unmarshal(&app)
+		if err != nil {
+			log.Println("error parse app dbconfig", err.Error())
+			return nil
+		}
+		JWTKey = app.JWTKey
+	}
+
+	return &app
+}
--- config/db.go
+++ config/db.go
@@ -1,1 +1,23 @@
 package config
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDB(dc DBConfig) *gorm.DB {
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", dc.DBUser, dc.DBPass, dc.DBHost, dc.DBPort, dc.DBName)
+	db, err := gorm.Open(mysql.Open(dataSourceName), &gorm.Config{})
+	if err != nil {
+		log.Println("database connection error : ", err.Error())
+		return nil
+	}
+	return db
+}
+
+func Migrate(db *gorm.DB) {
+	// db.AutoMigrate(uData.User{})
+}
